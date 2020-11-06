afa0f2710171225dc35299889d47d2736d199f4a
Rick <rick.yicong.li@gmail.com>
2020-11-06 20:24:14 +1100

    Add ViewControllers

--- WeatherInfo/ViewModels/WeatherListViewModel.swift
+++ WeatherInfo/ViewModels/WeatherListViewModel.swift
@@ -10,15 +10,16 @@ import Foundation
 class WeatherListViewModel {
     
     /// all cites' weather info
-    var orders = [WeatherInfoProperties]()
+    var cityList = [WeatherInfoProperties]()
     
     func fetchAllStoredCitiesInfo () {
         StorageManager.instance.getAllCitiesID { IDs in
+            
             ApiManager.instance.fetchInfoWith(cityIDs: IDs) { (weatherList) in
-                print(weatherList)
+                self.cityList = weatherList.list
             }
         }
-        
-        
     }
+    
+    
 }
